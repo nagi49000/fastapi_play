c2c6efbe2967629e43f6d14fa92f82f4304ae41e
nagi49000 <nagi49000@gmail.com>
2020-02-09 23:34:05 +0000

    itweaked logging, and added environment yml

--- api/api/server.py
+++ api/api/server.py
@@ -2,5 +2,6 @@ import logging
 from api.fastapi_app import create_app
 
 app = create_app()
-my_logger = logging.getLogger()
-my_logger.setLevel(logging.DEBUG)
+logging.basicConfig(filename=None,
+                    format='%(asctime)s %(levelname)-8s %(message)s',
+                    level=logging.DEBUG)
