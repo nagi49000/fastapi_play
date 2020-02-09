03932a2236ab78f97ad6e17660ffad37bf598052
nagi49000 <nagi49000@gmail.com>
2020-02-09 23:14:53 +0000

    added response models

--- api/api/fastapi_app.py
+++ api/api/fastapi_app.py
@@ -4,6 +4,10 @@ import logging
 from pydantic import BaseModel
 
 
+class HelloWorldResponse(BaseModel):
+    message: str
+
+
 class ParrotRequestParams(BaseModel):
     n_repeat: int
     sep: str
@@ -15,15 +19,25 @@ class ParrotRequest(BaseModel):
     parrot_request: ParrotRequestParams
 
 
+class ParrotBackResponseResults(BaseModel):
+    time: str
+    parrot: str
+
+
+class ParrotBackResponse(BaseModel):
+    header: str
+    results: ParrotBackResponseResults
+
+
 def create_app():
     app = FastAPI()
 
-    @app.get("/hello_world")
+    @app.get("/hello_world", response_model=HelloWorldResponse)
     async def hello_world():
         logging.debug('/hello_world')
         return {"message": "Hello World"}
 
-    @app.post("/parrot_back")
+    @app.post("/parrot_back", response_model=ParrotBackResponse)
     async def parrot_back(p: ParrotRequest):
         p_dict = p.dict()
         logging.debug('/parrot_back: '+str(p_dict))
