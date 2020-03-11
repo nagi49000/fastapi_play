51b4d5f0e8a611b196d4fcfe511dd5888eff407e
nagi49000 <nagi49000@gmail.com>
2020-03-11 06:54:57 +0000

    added filename option, and got creation of (empty) log file in docker-compose cmd

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,7 @@
 FROM continuumio/miniconda3
-SHELL ["/bin/bash", "-c"]
 
 WORKDIR /app
 COPY ./api /app
 RUN conda env create -f /app/environment.yml -n webenv
 
-SHELL ["conda", "run", "-n", "webenv", "/bin/bash", "-c"]
-ENTRYPOINT ["conda", "run", "-n", "webenv", "python", "api/server.py", "--host", "0.0.0.0", "--port", "8000"]
+ENTRYPOINT ["conda", "run", "-n", "webenv", "python", "api/server.py", "--host", "0.0.0.0", "--port", "8000", "--log-file", "/logs/api.log"]
--- api/api/server.py
+++ api/api/server.py
@@ -6,18 +6,22 @@ from api.fastapi_app import create_app
 
 app = create_app()
 
+
 def get_run_kwargs(command_line_args):
     parser = argparse.ArgumentParser()
     parser.add_argument('--host', help='URL for the server', default='127.0.0.1', type=str)
     parser.add_argument('--port', help='port number for the server', default=8000, type=int)
-    parser.add_argument('--log-level', help='level for logging in critical, error, warning, info, debug, trace', default='debug', type=str)
+    parser.add_argument('--log-file', help='file to write logging into', default=None, type=str)
+    parser.add_argument(
+        '--log-level', help='level for logging in critical, error, warning, info, debug, trace', default='debug', type=str)
     args = parser.parse_args(command_line_args)
     d = vars(args)
 
-    logging.basicConfig(filename=None,
+    logging.basicConfig(filename=d['log_file'],
                         format='%(asctime)s %(levelname)-8s %(message)s',
                         level=logging.DEBUG)
-    return d
+    return {k: d[k] for k in {'host', 'port', 'log_level'}}
+
 
 if __name__ == "__main__":
     kwargs = get_run_kwargs(sys.argv[1:])
