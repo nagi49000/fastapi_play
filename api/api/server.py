037acb14814287a8644c9dfca5f65ece5f8fc705
nagi49000 <nagi49000@gmail.com>
2020-03-12 18:13:20 +0000

    plumbed logging level command line arg

--- api/api/server.py
+++ api/api/server.py
@@ -9,17 +9,18 @@ app = create_app()
 
 def get_run_kwargs(command_line_args):
     parser = argparse.ArgumentParser()
-    parser.add_argument('--host', help='URL for the server', default='127.0.0.1', type=str)
-    parser.add_argument('--port', help='port number for the server', default=8000, type=int)
-    parser.add_argument('--log-file', help='file to write logging into', default=None, type=str)
-    parser.add_argument(
-        '--log-level', help='level for logging in critical, error, warning, info, debug, trace', default='debug', type=str)
+    parser.add_argument('--host', default='127.0.0.1', type=str, help='URL for the server')
+    parser.add_argument('--port', default=8000, type=int, help='port number for the server')
+    parser.add_argument('--log-file', default=None, type=str, help='file to write logging into')
+    parser.add_argument('--log-level', default='debug', type=str,
+                        choices=['critical', 'error', 'warning', 'info', 'debug'],
+                        help='level for logging in critical, error, warning, info, debug, trace')
     args = parser.parse_args(command_line_args)
     d = vars(args)
 
     logging.basicConfig(filename=d['log_file'],
                         format='%(asctime)s %(levelname)-8s %(message)s',
-                        level=logging.DEBUG)
+                        level=logging.getLevelName(d['log_level'].upper()))
     return {k: d[k] for k in {'host', 'port', 'log_level'}}
 
 
