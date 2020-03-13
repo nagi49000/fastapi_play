4b8d9a99a429295b9ebf849078ee3937f563c064
nagi49000 <nagi49000@gmail.com>
2020-03-13 19:53:46 +0000

    got shell script plumbed direct into docker-compose

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,1 @@
 FROM continuumio/miniconda3
-WORKDIR /api
-ENTRYPOINT ["bash", "run_server_in_docker.sh"]
--- api/run_server_in_docker.sh
+++ api/run_server_in_docker.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-conda env update -f /app/environment.yml -n server_in_docker_env 1>> /logs/conda_stdout.log 2>>/logs/conda_stderr.log
+cd /api
+conda env update -f /api/environment.yml -n server_in_docker_env 1>> /logs/conda_stdout.log 2>>/logs/conda_stderr.log
 conda run -n server_in_docker_env python api/server.py --host 0.0.0.0 --port 8000 1>> /logs/api_stdout.log 2>>/logs/api_stderr.log
