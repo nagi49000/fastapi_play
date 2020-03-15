062d30d097b29472113c7ef682fbf9c423f98f4b
nagi49000 <nagi49000@gmail.com>
2020-03-15 16:23:40 +0000

    got unit tests in container running. git log broken

--- api/run_server_in_docker.sh
+++ api/run_server_in_docker.sh
@@ -4,9 +4,11 @@ cd ${self_dir}
 # allow conda activate to be run in shell
 source /opt/conda/etc/profile.d/conda.sh 1>> /logs/conda_stdout.log 2>>/logs/conda_stderr.log
 # book-keeping
+git log -1 1>> /logs/git_stdout.log 2>> /logs/git_stderr.log
 conda --version 1>> /logs/conda_stdout.log 2>>/logs/conda_stderr.log
 # update environment from code on mountpoint
 conda env update -f environment.yml -n server_in_docker_env 1>> /logs/conda_stdout.log 2>>/logs/conda_stderr.log
 # activate environment and run service
 conda activate server_in_docker_env 1>> /logs/conda_stdout.log 2>>/logs/conda_stderr.log
-python api/server.py --host 0.0.0.0 --port 8000 1>> /logs/api_stdout.log 2>>/logs/api_stderr.log
+python -m pytest --cov=./api --cov-report=term-missing 1>> /logs/pytest_stdout.log 2>> /logs/pytest_stderr.log
+python api/server.py --host 0.0.0.0 --port 8000 --log-file /logs/api.log 1>> /logs/api_stdout.log 2>>/logs/api_stderr.log
