88c2f0aa6d0257b51c49a9e17c91e51db0e4b38e
nagi49000 <nagi49000@gmail.com>
2020-03-15 12:19:19 +0000

    clean up on run in docker script

--- api/run_server_in_docker.sh
+++ api/run_server_in_docker.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-cd /api
-conda env update -f /api/environment.yml -n server_in_docker_env 1>> /logs/conda_stdout.log 2>>/logs/conda_stderr.log
+self_dir=$(dirname $0)
+cd ${self_dir}
+conda env update -f environment.yml -n server_in_docker_env 1>> /logs/conda_stdout.log 2>>/logs/conda_stderr.log
 conda run -n server_in_docker_env python api/server.py --host 0.0.0.0 --port 8000 1>> /logs/api_stdout.log 2>>/logs/api_stderr.log
