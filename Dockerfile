45407a2716fc52a77b4d975669e2ee644760d0b4
nagi49000 <nagi49000@gmail.com>
2020-03-15 15:43:57 +0000

    got logs from api writing to mount point

--- Dockerfile
+++ Dockerfile
@@ -1,2 +1,3 @@
 FROM continuumio/miniconda3
+RUN chmod a+rw /root/.conda/environments.txt
 RUN conda update conda -c anaconda
--- api/run_server_in_docker.sh
+++ api/run_server_in_docker.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 self_dir=$(dirname $0)
 cd ${self_dir}
+# allow conda activate to be run in shell
+source /opt/conda/etc/profile.d/conda.sh 1>> /logs/conda_stdout.log 2>>/logs/conda_stderr.log
+# book-keeping
+conda --version 1>> /logs/conda_stdout.log 2>>/logs/conda_stderr.log
+# update environment from code on mountpoint
 conda env update -f environment.yml -n server_in_docker_env 1>> /logs/conda_stdout.log 2>>/logs/conda_stderr.log
-conda run -n server_in_docker_env python api/server.py --host 0.0.0.0 --port 8000 1>> /logs/api_stdout.log 2>>/logs/api_stderr.log
+# activate environment and run service
+conda activate server_in_docker_env 1>> /logs/conda_stdout.log 2>>/logs/conda_stderr.log
+python api/server.py --host 0.0.0.0 --port 8000 1>> /logs/api_stdout.log 2>>/logs/api_stderr.log
