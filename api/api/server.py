52cf7239e745f61d2720fee4f046402019bb1bc7
Jasbir Nagi <J.Nagi@ucas.ac.uk>
2020-03-16 13:39:25 +0000

    updated logging config

--- api/api/server.py
+++ api/api/server.py
@@ -7,6 +7,10 @@ from api.fastapi_app import create_app
 
 
 def get_run_kwargs(command_line_args):
+    """ command_line_args - list<str> - list of args as supplied fy sys.argv
+
+        returns a dict to initialise uvicorn.run
+    """
     parser = argparse.ArgumentParser()
     parser.add_argument('--host', default='127.0.0.1', type=str, help='URL for the server')
     parser.add_argument('--port', default=8000, type=int, help='port number for the server')
@@ -17,49 +21,51 @@ def get_run_kwargs(command_line_args):
     args = parser.parse_args(command_line_args)
     d = vars(args)
 
-    #logging.basicConfig(filename=d['log_file'],
-    #                    format='%(asctime)s %(levelname)-8s %(message)s',
-    #                    level=logging.getLevelName(d['log_level'].upper()))
-
-    run_kwargs = {k: d[k] for k in {'host', 'port', 'log_level'}}
+    run_kwargs = {k: d[k] for k in {'host', 'port'}}
     run_kwargs['log_config'] = get_log_config(logging.getLevelName(d['log_level'].upper()))
     return run_kwargs
 
-def get_log_config(debug_level):
+
+def get_log_config(log_level):
+    """ log_level - str - legitimate logging level string
+
+        returns a dict representing the level config. Bit of a
+        monster, since a full uvicorn logging config...
+    """
     d = {
-    "version": 1,
-    "disable_existing_loggers": False,
-    "formatters": {
-        "default": {
-            "()": "uvicorn.logging.DefaultFormatter",
-            "fmt": "%(levelprefix)s %(message)s",
-            "use_colors": None,
+        "version": 1,
+        "disable_existing_loggers": False,
+        "formatters": {
+            "default": {
+                "()": "uvicorn.logging.DefaultFormatter",
+                "fmt": "%(asctime)s %(levelname)-8s %(message)s",
+            },
+            "access": {
+                "()": "uvicorn.logging.AccessFormatter",
+                "fmt": '%(asctime)s %(levelname) %(client_addr)s - "%(request_line)s" %(status_code)s',
+            },
         },
-        "access": {
-            "()": "uvicorn.logging.AccessFormatter",
-            "fmt": '%(levelprefix)s %(client_addr)s - "%(request_line)s" %(status_code)s',
+        "handlers": {
+            "default": {
+                "formatter": "default",
+                "class": "logging.StreamHandler",
+                "stream": "ext://sys.stdout",
+            },
+            "access": {
+                "formatter": "access",
+                "class": "logging.StreamHandler",
+                "stream": "ext://sys.stdout",
+            },
         },
-    },
-    "handlers": {
-        "default": {
-            "formatter": "default",
-            "class": "logging.StreamHandler",
-            "stream": "ext://sys.stdout",
+        "loggers": {
+            "": {"handlers": ["default"], "level": log_level},
+            "uvicorn.error": {"level": log_level},
+            "uvicorn.access": {"handlers": ["access"], "level": log_level, "propagate": False},
         },
-        "access": {
-            "formatter": "access",
-            "class": "logging.StreamHandler",
-            "stream": "ext://sys.stdout",
-        },
-    },
-    "loggers": {
-        "": {"handlers": ["default"], "level": "INFO"},
-        "uvicorn.error": {"level": "INFO"},
-        "uvicorn.access": {"handlers": ["access"], "level": "INFO", "propagate": False},
-    },
-}
+    }
     return d
-    
+
+
 app = create_app()
 
 
--- api/test/test_server.py
+++ api/test/test_server.py
@@ -3,10 +3,59 @@ from api.server import get_run_kwargs
 
 def test_get_run_kwargs():
     a = get_run_kwargs([])
-    assert a == {'host': '127.0.0.1', 'log_level': 'debug', 'port': 8000}
+    assert a == {'host': '127.0.0.1',
+                 'port': 8000,
+                 'log_config': {'disable_existing_loggers': False,
+                                'formatters': {'access': {'()': 'uvicorn.logging.AccessFormatter',
+                                                          'fmt': '%(asctime)s %(levelname) '
+                                                          '%(client_addr)s - '
+                                                          '"%(request_line)s" '
+                                                          '%(status_code)s'},
+                                               'default': {'()': 'uvicorn.logging.DefaultFormatter',
+                                                           'fmt': '%(asctime)s %(levelname)-8s '
+                                                           '%(message)s'}
+                                               },
+                                'handlers': {'access': {'class': 'logging.StreamHandler',
+                                                        'formatter': 'access',
+                                                        'stream': 'ext://sys.stdout'},
+                                             'default': {'class': 'logging.StreamHandler',
+                                                         'formatter': 'default',
+                                                         'stream': 'ext://sys.stdout'}
+                                             },
+                                'loggers': {'': {'handlers': ['default'], 'level': 10},
+                                            'uvicorn.access': {'handlers': ['access'],
+                                                               'level': 10,
+                                                               'propagate': False},
+                                            'uvicorn.error': {'level': 10}
+                                            },
+                                'version': 1}
+                 }
 
     a = get_run_kwargs(['--host', '0.0.0.0',
                         '--port', '1234',
                         '--log-level', 'warning',
                         '--log-file', 'log.txt'])
-    assert a == {'host': '0.0.0.0', 'log_level': 'warning', 'port': 1234}
+    assert a == {'host': '0.0.0.0',
+                 'port': 1234,
+                 'log_config': {'disable_existing_loggers': False,
+                                'formatters': {'access': {'()': 'uvicorn.logging.AccessFormatter',
+                                                          'fmt': '%(asctime)s %(levelname) %(client_addr)s - "%(request_line)s" %(status_code)s'},
+                                               'default': {'()': 'uvicorn.logging.DefaultFormatter',
+                                                           'fmt': '%(asctime)s %(levelname)-8s %(message)s'}
+                                               },
+                                'handlers': {'access': {'class': 'logging.StreamHandler',
+                                                        'formatter': 'access',
+                                                        'stream': 'ext://sys.stdout'},
+                                             'default': {'class': 'logging.StreamHandler',
+                                                         'formatter': 'default',
+                                                         'stream': 'ext://sys.stdout'}
+                                             },
+                                'loggers': {'': {'handlers': ['default'],
+                                                 'level': 30},
+                                            'uvicorn.access': {'handlers': ['access'],
+                                                               'level': 30,
+                                                               'propagate': False},
+                                            'uvicorn.error': {'level': 30}
+                                            },
+                                'version': 1},
+                 }
