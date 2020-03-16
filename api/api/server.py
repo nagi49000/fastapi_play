538d058ccde293abfd3714cd7a2cfcfc536566f2
nagi49000 <nagi49000@gmail.com>
2020-03-16 12:55:32 +0000

    working on logging in uvicorn fixes

--- api/api/server.py
+++ api/api/server.py
@@ -1,10 +1,9 @@
+import time
 import sys
 import argparse
 import uvicorn
 import logging
 from api.fastapi_app import create_app
-
-app = create_app()
 
 
 def get_run_kwargs(command_line_args):
@@ -18,10 +17,50 @@ def get_run_kwargs(command_line_args):
     args = parser.parse_args(command_line_args)
     d = vars(args)
 
-    logging.basicConfig(filename=d['log_file'],
-                        format='%(asctime)s %(levelname)-8s %(message)s',
-                        level=logging.getLevelName(d['log_level'].upper()))
-    return {k: d[k] for k in {'host', 'port', 'log_level'}}
+    #logging.basicConfig(filename=d['log_file'],
+    #                    format='%(asctime)s %(levelname)-8s %(message)s',
+    #                    level=logging.getLevelName(d['log_level'].upper()))
+
+    run_kwargs = {k: d[k] for k in {'host', 'port', 'log_level'}}
+    run_kwargs['log_config'] = get_log_config(logging.getLevelName(d['log_level'].upper()))
+    return run_kwargs
+
+def get_log_config(debug_level):
+    d = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "formatters": {
+        "default": {
+            "()": "uvicorn.logging.DefaultFormatter",
+            "fmt": "%(levelprefix)s %(message)s",
+            "use_colors": None,
+        },
+        "access": {
+            "()": "uvicorn.logging.AccessFormatter",
+            "fmt": '%(levelprefix)s %(client_addr)s - "%(request_line)s" %(status_code)s',
+        },
+    },
+    "handlers": {
+        "default": {
+            "formatter": "default",
+            "class": "logging.StreamHandler",
+            "stream": "ext://sys.stdout",
+        },
+        "access": {
+            "formatter": "access",
+            "class": "logging.StreamHandler",
+            "stream": "ext://sys.stdout",
+        },
+    },
+    "loggers": {
+        "": {"handlers": ["default"], "level": "INFO"},
+        "uvicorn.error": {"level": "INFO"},
+        "uvicorn.access": {"handlers": ["access"], "level": "INFO", "propagate": False},
+    },
+}
+    return d
+    
+app = create_app()
 
 
 if __name__ == "__main__":
