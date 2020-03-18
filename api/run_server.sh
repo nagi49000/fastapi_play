8cec482daec7f697d1cf3af811bd14a0f576d501
nagi49000 <nagi49000@gmail.com>
2020-03-18 18:25:54 +0000

    removed server.py boiler plating

--- api/run_server.sh
+++ api/run_server.sh
@@ -1,2 +1,2 @@
-export PYTHONPATH=${PYTHONPATH}:`pwd`
-python api/server.py
+cd ./api
+uvicorn fastapi_app:app --host 0.0.0.0 --port 8000 --log-config log_config.ini
--- api/run_server_in_docker.sh
+++ api/run_server_in_docker.sh
@@ -11,4 +11,5 @@ conda env update -f environment.yml -n server_in_docker_env 1>> /logs/conda_stdo
 # activate environment and run service
 conda activate server_in_docker_env 1>> /logs/conda_stdout.log 2>>/logs/conda_stderr.log
 python -m pytest --cov=./api --cov-report=term-missing 1>> /logs/pytest_stdout.log 2>> /logs/pytest_stderr.log
-python api/server.py --host 0.0.0.0 --port 8000 --log-file /logs/api.log 1>> /logs/api_stdout.log 2>>/logs/api_stderr.log
+cd ./api
+uvicorn fastapi_app:app --host 0.0.0.0 --port 8000 --log-config log_config.ini --log-level debug 1>> /logs/api_stdout.log 2>>/logs/api_stderr.log
